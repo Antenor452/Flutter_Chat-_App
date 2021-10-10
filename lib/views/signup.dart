7131b9b6b1c409be9f54accc54df3a38b3c574ab
Antenor <nanaosei452@gmail.com>
2021-10-10 17:30:44 +0000

    fixed google-services json

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -16,94 +16,96 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
-      body: Container(
-        height: MediaQuery.of(context).size.height - 50,
-        alignment: Alignment.bottomCenter,
+      body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: userNameController,
-                decoration: textFieldInputDecoration('Username'),
-                style: simpleTextStyle(),
-              ),
-              TextField(
-                controller: emailController,
-                decoration: textFieldInputDecoration('Email'),
-                style: simpleTextStyle(),
-              ),
-              TextField(
-                  controller: passwordController,
-                  decoration: textFieldInputDecoration('Password'),
-                  style: simpleTextStyle()),
-              const SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Text(
-                    'Forgot Password?',
-                    style: simpleTextStyle(),
+          height: MediaQuery.of(context).size.height - 50,
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: userNameController,
+                  decoration: textFieldInputDecoration('Username'),
+                  style: simpleTextStyle(),
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: textFieldInputDecoration('Email'),
+                  style: simpleTextStyle(),
+                ),
+                TextField(
+                    controller: passwordController,
+                    decoration: textFieldInputDecoration('Password'),
+                    style: simpleTextStyle()),
+                const SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Text(
+                      'Forgot Password?',
+                      style: simpleTextStyle(),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                decoration: BoxDecoration(
+                const SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: const LinearGradient(
+                          colors: [Color(0xff007eF4), Color(0xff2A75BC)])),
+                  child: Text('Sign up', style: meduimTextStyle()),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(30),
-                    gradient: const LinearGradient(
-                        colors: [Color(0xff007eF4), Color(0xff2A75BC)])),
-                child: Text('Sign up', style: meduimTextStyle()),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.white,
-                ),
-                child: const Text(
-                  'Sign up with Google',
-                  style: TextStyle(fontSize: 17, color: Colors.black87),
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account?",
-                    style: meduimTextStyle(),
+                    color: Colors.white,
                   ),
-                  const Text(
-                    "Sign In Now",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 17,
-                        decoration: TextDecoration.underline),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 100,
-              )
-            ],
+                  child: const Text(
+                    'Sign up with Google',
+                    style: TextStyle(fontSize: 17, color: Colors.black87),
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: meduimTextStyle(),
+                    ),
+                    const Text(
+                      "Sign In Now",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          decoration: TextDecoration.underline),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 100,
+                )
+              ],
+            ),
           ),
         ),
       ),
