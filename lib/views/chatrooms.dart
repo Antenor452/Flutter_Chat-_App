54bd30de6644807cff0eb7465fc7dbad0f00af4c
Antenor <nanaosei452@gmail.com>
2021-10-15 17:46:19 +0000

    Adding Search ile

--- lib/views/chatrooms.dart
+++ lib/views/chatrooms.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/helper/authenticate.dart';
+import 'package:chat_app/views/search.dart';
 import 'package:chat_app/views/signin.dart';
 import 'package:chat_app/widgets/widget.dart';
 import 'package:flutter/material.dart';
@@ -16,23 +17,31 @@ class _ChatRoomState extends State<ChatRoom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Image.asset(
-        "assets/images/logo.png",
-        height: 40,
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/logo.png",
+          height: 40,
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                authMethods.signOut().then((value) {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Authenticate()));
+                });
+              },
+              icon: const Icon(Icons.exit_to_app))
+        ],
       ),
-      actions: [
-        IconButton(
-            onPressed: () {
-              authMethods.signOut().then((value) {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const Authenticate()));
-              });
-            },
-            icon: const Icon(Icons.exit_to_app))
-      ],
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SearchScreen()));
+        },
+        child: const Icon(Icons.search),
+      ),
+    );
   }
 }
---
+++ lib/views/search.dart
@@ -0,0 +1,18 @@
+import 'package:chat_app/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+    );
+  }
+}
