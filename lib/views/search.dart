0a8824a3a197fd4a325e86e842c5d21bf13d60f7
Antenor <nanaosei452@gmail.com>
2021-10-15 18:06:44 +0000

    Creating databasemethods

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -9,10 +9,42 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchtext = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
+      body: Container(
+        child: Column(children: [
+          Container(
+            color: const Color(0x54FFFFFF),
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            child: Row(
+              children: [
+                Expanded(
+                    child: TextFormField(
+                        controller: searchtext,
+                        style: const TextStyle(color: Colors.white),
+                        decoration: const InputDecoration(
+                            hintText: "Search username",
+                            hintStyle: TextStyle(color: Colors.white54),
+                            border: InputBorder.none))),
+                Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        gradient: const LinearGradient(colors: [
+                          Color(0x36FFFFFF),
+                          Color(0x0FFFFFFF),
+                        ]),
+                        borderRadius: BorderRadius.circular(48)),
+                    padding: const EdgeInsets.all(12),
+                    child: Image.asset("assets/images/search_white.png"))
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
