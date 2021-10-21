b2204f381582ee396e75f3d5666d1cf15ae88220
Antenor <nanaosei452@gmail.com>
2021-10-21 17:06:56 +0000

    get sharedpref

--- lib/helper/helperfunctions.dart
+++ lib/helper/helperfunctions.dart
@@ -12,4 +12,30 @@ class HelperFunctions {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return await prefs.setBool(sharedPreferenceUserLoggedInKey, isUserLoggedIn);
   }
+
+  static Future<bool> saveusernameSharedPreference(String username) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.setString(sharedPreferenceUsernameKey, username);
+  }
+
+  static Future<bool> saveuserEmailSharedPreference(String userEmail) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.setString(sharedPreferenceUserEmailKey, userEmail);
+  }
+
+  //get data from shared pref
+  static Future<bool?> getuserLoggedInSharedPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.getBool(sharedPreferenceUserLoggedInKey);
+  }
+
+  static Future<String?> getusernameSharedPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.getString(sharedPreferenceUsernameKey);
+  }
+
+  static Future<String?> getuserEmailSharedPreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return await prefs.getString(sharedPreferenceUserEmailKey);
+  }
 }
